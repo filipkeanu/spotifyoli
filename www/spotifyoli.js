71b67ce23a235a0961982a77e4a355b6b9048a4d
Filip_Keanu <knapikfilip@gmail.com>
2016-10-23 18:03:35 +0200

    Update spotifyoli.js

--- www/spotifyoli.js
+++ www/spotifyoli.js
@@ -2,6 +2,6 @@
 
 module.exports = {
     greet: function (name, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "Spotifyoli", "greet", [name]);
+        cordova.exec(successCallback, errorCallback, "spotifyoli", "greet", [name]);
     }
 };
